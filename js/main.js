4fac2ae51eeb461ee52d0655428fe42a72193b02
Shuvo Habib <shuvohabib@gmail.com>
2015-10-05 19:10:05 +0600

    Beta
    Available for testing

--- js/main.js
+++ js/main.js
@@ -8,37 +8,20 @@
 //Testimonial
 
 
-    $(function() {
-        $('#testtext-1').show( "slow" );
-        $('#testtext-2').hide();
-        $('#testtext-3').hide();
-        $('div.test-2').addClass('changeafter');
-        $('div.test-3').addClass('changeafter');
+$(function() {
+    $('.text-testimonial').hide();
+    $('#testimonial-1').show( "slow" );
 
-    $('#test-1').click(function(){
-        $('#testtext-1').show( "slow" );
-        $('#testtext-2').hide();
-        $('#testtext-3').hide();
-        $('div.test-1').removeClass('changeafter');
-        $('div.test-2').addClass('changeafter');
-        $('div.test-3').addClass('changeafter');
+    $('.client').on('click', function(e){
+        e.preventDefault();
+        var testimonial = $(this).data('testimonial_id');
+        $('.text-testimonial').hide();
+        $('#'+testimonial).show( "slow" );
+        $('.client').removeClass('redmark');
+        $(this).addClass('redmark');
     });
-    $('#test-2').click(function(){
-        $('#testtext-1').hide();
-        $('#testtext-2').show( "slow" );
-        $('#testtext-3').hide();
-        $('div.test-2').removeClass('changeafter');
-        $('div.test-1').addClass('changeafter');
-        $('div.test-3').addClass('changeafter');
-    });
-    $('#test-3').click(function(){
-        $('#testtext-1').hide();
-        $('#testtext-2').hide();
-        $('#testtext-3').show( "slow" );
-        $('div.test-3').removeClass('changeafter');
-        $('div.test-2').addClass('changeafter');
-        $('div.test-1').addClass('changeafter');
-    });
+
+
 });
 
 
